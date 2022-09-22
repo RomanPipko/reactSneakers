6b51aba68864bd7e8d1b0b4a4bce0c374415d304
User <roman.pipko@yandex.ru>
2022-09-22 18:18:50 +0300

    верстка

--- src/App.js
+++ src/App.js
@@ -21,7 +21,16 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search"></img>
+            <input placeholder="Поиск"></input>
+
+          </div>
+
+        </div>
+        
         <div className="d-flex">
           <div className="card">
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"></img>
