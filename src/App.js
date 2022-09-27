563920c7dca6b6441fdb449008319c691e4a09f7
User <roman.pipko@yandex.ru>
2022-09-27 19:47:20 +0300

    рендеринг компонента Card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from './components/Card';
 import Drawer from './components/Drawer';
 import Header from './components/Header';
 
+const arr = [
+{title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
+{title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg'},
+{title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+{title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'}
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -16,13 +23,14 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card/>
-          <Card/>
-          <Card/>
+          {arr.map(obj => <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl}/>)}
         </div>
       </div>
     </div>
   );
 }
+
+
+
 
 export default App;
